3b627afe3c16e6cad4627b0d53db0b6fbe3e4b3c
zzzpz <1911293650@qq.com>
2025-10-24 10:57:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ def PrimeList(N):
     if N <= 2:
         return ""
     primes = []
-    # 从2开始判断，2是质数
     for num in range(2, N):
         is_prime = True
-        # 优化：除数只需遍历到num的平方根
-        for i in range(2, int(num**0.5) + 1):
+        for i in range(2, int(num ** 0.5) + 1):
             if num % i == 0:
                 is_prime = False
                 break
