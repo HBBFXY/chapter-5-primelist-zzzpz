01196d8dd46a0996f34e6ec537b8b00591b19fd8
zzzpz <1911293650@qq.com>
2025-10-26 14:08:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ def is_prime(num):
         if num % i == 0:
             return False
     return True
+
+def PrimeList(N):
+    primes = []
+    for num in range(2, N):
+        if is_prime(num):
+            primes.append(str(num))
+    return ' '.join(primes)
